89632f3e7c4c09655d2b75d3805e21bdd85c661b
developiaa <developiaa@gmail.com>
2021-11-09 20:24:27 +0900

    feat : Add basic sign up

--- src/main/java/study/developia/login/TestDataInit.java
+++ src/main/java/study/developia/login/TestDataInit.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import study.developia.login.domain.item.Item;
 import study.developia.login.domain.item.ItemRepository;
+import study.developia.login.domain.member.Member;
+import study.developia.login.domain.member.MemberRepository;
 
 import javax.annotation.PostConstruct;
 
@@ -12,6 +14,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -20,6 +23,13 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+        memberRepository.save(member);
     }
 
 }
